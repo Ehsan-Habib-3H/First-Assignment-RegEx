2a19194c80a5cd379a0549378b4441c660f4a89d
Ξhsan (3H) <43131138+Ehsan-Habib-3H@users.noreply.github.com>
2024-02-28 23:02:53 +0330

    completed

--- src/main/java/Exercise3.java
+++ src/main/java/Exercise3.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -11,15 +12,31 @@ public class Exercise3 {
     */
 
     public static String extractURL(String text) {
-        String regex = "write your regex pattern here!";  // TODO
-
-        Pattern pattern = Pattern.compile(regex);
+        String regex = "((?:(http|https|Http|Https):\\/\\/"
+                + "(?:(?:[a-zA-Z0-9\\$\\-\\_\\.\\+\\!\\*\\'\\(\\)\\,\\;\\?\\&\\=]|"
+                + "(?:\\%[a-fA-F0-9]{2})){1,64}(?:\\:(?:[a-zA-Z0-9\\$\\-\\_\\.\\"
+                + "+\\!\\*\\'\\(\\)\\,\\;\\?\\&\\=]|(?:\\%[a-fA-F0-9]{2})){1,25})"
+                + "?\\@)?)?((?:(?:[a-zA-Z0-9][a-zA-Z0-9\\-\\_]{0,64}\\.)"
+                + "+(?:(?:aero|arpa|asia|a[cdefgilmnoqrstuwxz])"
+                + "|(?:biz|b[abdefghijmnorstvwyz])|(?:cat|com|"
+                + "coop|c[acdfghiklmnoruvxyz])|d[ejkmoz]|(?:edu|e[cegrstu])|f[ijkmor]"
+                + "|(?:gov|g[abdefghilmnpqrstuwy])|h[kmnrtu]|(?:info|int|i[delmnoqrst])"
+                + "|(?:jobs|j[emop])|k[eghimnrwyz]|l[abcikrstuvy]|"
+                + "(?:mil|mobi|museum|m[acdeghklmnopqrstuvwxyz])|"
+                + "(?:name|net|n[acefgilopruz])|(?:org|om)|(?:pro|p[aefghklmnrstwy])|"
+                + "qa|r[eouw]|s[abcdeghijklmnortuvyz]|(?:tel|travel|t[cdfghjklmnoprtvwz])"
+                + "|u[agkmsyz]|v[aceginu]|w[fs]|y[etu]|z[amw]))|(?:(?:25[0-5]|2[0-4][0-9]"
+                + "|[0-1][0-9]{2}|[1-9][0-9]|[1-9])\\.(?:25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}"
+                + "|[1-9][0-9]|[1-9]|0)\\.(?:25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[1-9][0-9]"
+                + "|[1-9]|0)\\.(?:25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[1-9][0-9]|[0-9])))"
+                + "(?:\\:\\d{1,5})?)(\\/(?:(?:[a-zA-Z0-9\\;\\/\\?\\:\\@\\&\\=\\#\\~%\\-\\."
+                + "\\+\\!\\*\\'\\(\\)\\,\\_])|(?:\\%[a-fA-F0-9]{2}))*)?";
+        Pattern pattern = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
         Matcher matcher = pattern.matcher(text);
 
         if (matcher.find()) {
             return matcher.group();
-        }
-        else{
+        } else {
             return null;
         }
     }
@@ -29,8 +46,12 @@ public class Exercise3 {
      */
 
     public static boolean validateEmail(String email) {
-        // TODO
-        return false;
+        String regex = "^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@"
+                + "[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$";
+        Pattern pattern = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(email);
+
+        return matcher.matches();
     }
 
     /*
@@ -39,8 +60,15 @@ public class Exercise3 {
 
     public static List<String> findWordsWithRepeatLetters(String input) {
         List<String> wordsWithRepeatLetters = new ArrayList<>();
+        input = input.replaceAll("[^a-zA-Z0-9| ]", "");// for removing non-alphabeltic chars
+        String[] words = input.split("\\s+");//Spliting into array
+        for (String word : words) {
+            if (Pattern.compile("\\b([A-Za-z]{2,})\\1\\b").matcher(word).find()) {
+                wordsWithRepeatLetters.add(word);
+                System.out.println(word);
+            }
+        }
         return wordsWithRepeatLetters;
-        // TODO
     }
 
     /*
@@ -51,8 +79,11 @@ public class Exercise3 {
 
     public static List<String> findReapetdWords(String input) {
         List<String> repeatedWords = new ArrayList<>();
+        Matcher match = Pattern.compile("\\b([A-Za-z]{2,})\\1\\b").matcher(input);// Here I complied the pattern and put it in Matcher simultaneously (:
+        while (match.find()) {
+            repeatedWords.add(match.group());
+        }
         return repeatedWords;
-        // TODO
     }
 
     public static void main(String[] args) {
